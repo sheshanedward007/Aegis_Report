41ac7a54cf0f40f897af49b09fd3a2dc4fcc0e4b
sheshanedward007 <sheshanedward007@gmail.com>
2025-12-13 21:13:53 +0530

    Firebase setup

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmxz5b598PlSbUyKbh4VwHvRpAxltqcFA",
+    authDomain: "project-aegis-845e6.firebaseapp.com",
+    projectId: "project-aegis-845e6",
+    storageBucket: "project-aegis-845e6.firebasestorage.app",
+    messagingSenderId: "851940559314",
+    appId: "1:851940559314:web:a2d93e9d366577855a200b"
+};
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
